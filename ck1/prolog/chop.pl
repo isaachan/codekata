546b51fc23aeb12b4f0344b7ee2584dd9b4701b1
Isaac Han <isaachanstar@gmail.com>
2013-10-07 15:35:35 +0800

    5th test

--- ck1/prolog/chop.pl
+++ ck1/prolog/chop.pl
@@ -5,10 +5,17 @@ chop(Index, E, List) :-
   length(List, Length),
   chop(Index, E, List, 0, Length).
 
-chop(MiddleIndex, E, List, Start, End) :-
-  Length is End -Start,
-  MiddleIndex is Length / 2,
+chop(MiddleIndex, MiddleElement, List, Start, End) :-
+  Length is End - Start, 
+  MiddleIndex is div(Length, 2), 
   indexOf(MiddleElement, MiddleIndex, List).
+
+chop(Index, E, List, Start, End) :-
+  Length is End -Start,
+  MiddleIndex is div(Length, 2),
+  indexOf(MiddleElement, MiddleIndex, List),
+  MiddleElement > E,
+  chop(Index, E, List, Start, MiddleIndex).
 
 indexOf(E, 0, [E|_]) :- !.
 indexOf(E, N, [_|R]) :- N1 is N-1, indexOf(E, N1, R), !.
