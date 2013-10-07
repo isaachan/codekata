2e8c0c984eb52d76a7ff626a8670a2e18d24cdab
Isaac Han <isaachanstar@gmail.com>
2013-10-07 16:35:17 +0800

    2nd test

--- ck1/ruby/chop.rb
+++ ck1/ruby/chop.rb
@@ -1,3 +1,4 @@
 def chop(element, list)
-  return -1
+  return -1 if list.length == 0
+  return 0 if list.length == 1
 end
--- ck1/ruby/chop_test.rb
+++ ck1/ruby/chop_test.rb
@@ -4,7 +4,6 @@ load './chop.rb'
 class ChopTest < Test::Unit::TestCase
   def test_chop
     assert_equal(-1, chop(1, []))
+    assert_equal(0, chop(10, [10]))
   end
 end
-
-
