86aa6efe3fe844c107d521886f64e565e2f78f4e
Isaac Han <isaachanstar@gmail.com>
2013-10-07 15:56:25 +0800

    add more tests and fix a defect

--- ck1/prolog/chop.pl
+++ ck1/prolog/chop.pl
@@ -22,7 +22,7 @@ chop(Index, E, List, Start, End) :-
   MiddleIndex is div(Length, 2) + Start,
   indexOf(MiddleElement, MiddleIndex, List),
   MiddleElement < E,
-  chop(Index, E, List, MiddleIndex, Length).
+  chop(Index, E, List, MiddleIndex, End).
 
 indexOf(E, 0, [E|_]) :- !.
 indexOf(E, N, [_|R]) :- N1 is N-1, indexOf(E, N1, R), !.
