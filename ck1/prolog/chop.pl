32f0fdaeb433f2bdcb9fd227a1a96d58605e8ff6
Isaac Han <isaachanstar@gmail.com>
2013-10-07 14:28:14 +0800

    add indexOf predicate

--- ck1/prolog/chop.pl
+++ ck1/prolog/chop.pl
@@ -1,2 +1,6 @@
 chop(-1, _, []).
 chop(0, E, [E]).
+
+
+indexOf(E, 0, [E|_]).
+indexOf(E, N, [_|R]) :- N1 is N-1, indexOf(E, N1, R).
--- ck1/prolog/chop_test.pl
+++ ck1/prolog/chop_test.pl
@@ -1,5 +1,8 @@
 :- ['chop.pl'].
 
 test :-
+  indexOf(a, 0, [a,b,c]),
+  indexOf(c, 2, [a,b,c]),
   chop(-1, 1, []),
   chop(0, 1, [1]).
+
