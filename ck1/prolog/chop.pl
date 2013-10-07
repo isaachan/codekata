30cb8e2d6d87c8880346a9353997d39735ef84d7
Isaac Han <isaachanstar@gmail.com>
2013-10-07 14:44:26 +0800

    4th test

--- ck1/prolog/chop.pl
+++ ck1/prolog/chop.pl
@@ -1,6 +1,10 @@
 chop(-1, _, []).
 chop(0, E, [E]).
 
+chop(MiddleIndex, E, List) :- ,
+  length(List, Length),
+  MiddleIndex is Length / 2,
+  indexOf(MiddleElement, MiddleIndex, List).
 
-indexOf(E, 0, [E|_]).
-indexOf(E, N, [_|R]) :- N1 is N-1, indexOf(E, N1, R).
+indexOf(E, 0, [E|_]) :- !.
+indexOf(E, N, [_|R]) :- N1 is N-1, indexOf(E, N1, R), !.
--- ck1/prolog/chop_test.pl
+++ ck1/prolog/chop_test.pl
@@ -3,6 +3,8 @@
 test :-
   indexOf(a, 0, [a,b,c]),
   indexOf(c, 2, [a,b,c]),
+
   chop(-1, 1, []),
-  chop(0, 1, [1]).
+  chop(0, 10, [10]),
+  chop(1, 11, [10, 11]).
 
